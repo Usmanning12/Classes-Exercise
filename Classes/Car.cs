26c72cc14eca1893ae5e51ffab95a1961831a467
Darron Manning <darronmanning@yahoo.com>
2025-05-18 11:46:43 -0500

    finished Classes Exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,11 @@
+namespace Classes
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+    }
+
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Classes
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var CarOne = new Car();
+            CarOne.Make = "Ford";
+            CarOne.Model = "Bronco";
+            CarOne.Year = 2019;
+            Console.WriteLine($"This is {CarOne.Make},{CarOne.Model},it was made in the year {CarOne.Year}.");
         }
     }
 }
+    
+
